1385bc5350450e5effc1e3d251d9f5dd5b8f0a2c
Surendran Mahendran <surenspost@gmail.com>
2014-09-06 21:48:27 -0400

    Fix typo with partial naming

--- app/overrides/show_fulfillment_date_in_orders.rb
+++ app/overrides/show_fulfillment_date_in_orders.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/orders/_shipment',
   name: 'add_fulfillment_date_in_orders',
-  insert_after: "",
-  partial: 'spree/admin/orders/fullfillment',
+  insert_after: ".show-tracking",
+  partial: 'spree/admin/orders/fulfillment',
   disabled: false
 )
