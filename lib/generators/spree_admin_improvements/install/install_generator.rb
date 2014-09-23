af5457147917abec1912c3579b9871a4eb96d064
Surendran Mahendran <surenspost@gmail.com>
2014-09-23 01:54:52 -0400

    Remove store related assets files

--- lib/generators/spree_admin_improvements/install/install_generator.rb
+++ lib/generators/spree_admin_improvements/install/install_generator.rb
@@ -5,13 +5,11 @@ module SpreeAdminImprovements
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_admin_improvements\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_admin_improvements\n"
+        append_file 'app/assets/javascripts/admin/all.js', "\n//= require admin/spree_admin_improvements\n"
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_admin_improvements\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_admin_improvements\n", :before => /\*\//, :verbose => true
+        inject_into_file 'app/assets/stylesheets/admin/all.css', "\n *= require admin/spree_admin_improvements\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
